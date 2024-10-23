e0283dcf68fa3abb8d5d1c2682379346b043913f
Kevmill29 <kev29@fanaticalkev.com>
2024-10-23 17:38:21 -0400

    Add existing file

--- src/LabEight.java
+++ src/LabEight.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class LabEight {
@@ -6,9 +7,9 @@ public class LabEight {
 
         MyName namePrint = new MyName();
         System.out.print("1.--------------------");
-       for(int q= 0; q<3; q++){
-           namePrint.printName();
-       }
+        for (int q = 0; q < 3; q++) {
+            namePrint.printName();
+        }
 
         System.out.print("\n2 & 3.---------------------\nEnter a word: ");
 
@@ -25,6 +26,7 @@ public class LabEight {
         System.out.print("\n Enter your height in inches: ");
         double height = input.nextDouble();
         bmi = bmiCalc(weight, height);
+        input.nextLine();
         System.out.print("Your BMI is: " + bmi);
         System.out.print("\n6.------------------------------\n");
         int seconds = 313297;
@@ -38,15 +40,19 @@ public class LabEight {
         int b = 5;
 
         pythagTheorem(a, b);
-        System.out.print("\n8.--------------------\nEnter your first name : ");
-        String firstName = input.next();
-        System.out.print("Enter your last name: ");
-        String lastName = input.next();
-        String fullName = nameEntered(firstName, lastName);
-        System.out.print("Your full name is " + fullName);
+        System.out.print("\n8.--------------------\nEnter your first name and last name : ");
+        String[] name = input.nextLine().split(" ");
+
+
+        String fullName = nameEntered(name);
+        System.out.print("Your full name is:\n" + fullName);
+
+        input.nextLine();
         System.out.print("\n9.-------------------------\nEnter a number:");
+
         int num = input.nextInt();
-        int factor = factorial(num);
+        BigInteger factor = factorial(num);
+
         System.out.print("The factorial of " + num + " is " + factor);
         System.out.print("\n10.----------------\nEnter a number to see if its a prime number: ");
         int p = input.nextInt();
@@ -102,33 +108,32 @@ public class LabEight {
     }
 
     //Question 8
-    public static String nameEntered(String firstName, String lastName) {
-        return firstName + " " + lastName;
+    public static String nameEntered(String[] name) {
+        if (name.length < 2) {
+            return "Error: You must enter both a first and last name.";
+        } else if (name.length > 2) {
+            return "Error: Too many words entered. Please enter only first and last name.";
+        } else {
+            return "Full Name: " + name[0] + " " + name[1];
+        }
     }
 
     //Question 9
-    public static int factorial(int num) {
-        int sum = 0;
-        for (int i = 0; i < num; i++) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
-        }
-        return sum;
+    public static BigInteger factorial(int num) {
+     if(num<=1) return BigInteger.ONE;
+     return BigInteger.valueOf(num).multiply(factorial(num-1));
     }
 
     //Question 10
     public static boolean primeNum(int p) {
-        if (p < 1) {
-            return false;
-        }else if (p == 1){
-            return true;
-        }
-        for (int i = 2; i < p; i++) {
-            if (p % i == 0) {
-                return false; //not prime if it is divisible by a number other than itself
+        if (p <=1) return false;
+        if (p==2)return true; //2 is the only prime number excluded
+        if (p % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(p); i+=2) {
+            //not prime if it is divisible by a number other than itself
+            if(p % i ==0)return false;
             }
-        }
+
         return true;
     }
 }
